2ba4d0dddcf176703dc531dba153df4457b30c1d
valeriy <viaya.lysenko@mail.ru>
2026-02-21 13:14:01 +0300

    changes config

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,8 @@
-import {defineConfig} from "vite";
-import react from "@vitejs/plugin-react";
+import {defineConfig} from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
     base: '/DmitriyBMW/',
-    plugins: [
-        react({
-            babel: {
-                plugins: ["babel-plugin-styled-components"],
-            },
-        }),
-    ],
-    resolve: {
-        alias: {
-            "@": "/src",
-        },
-    }
-});
+    plugins: [react()],
+})
